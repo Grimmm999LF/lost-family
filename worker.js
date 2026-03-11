a77cc19be5568ea124fdecac077cedda6efab4d8
Grimmm999LF <grimmm999@gmail.com>
2026-03-11 09:35:28 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,40 +1,44 @@
 export default {
   async fetch(request) {
 
+    // pozwól tylko na POST
+    if (request.method !== "POST") {
+      return new Response("Method not allowed", { status: 405 });
+    }
+
     const data = await request.json();
 
-    // 🔐 zabezpieczenie
+    // zabezpieczenie
     if (data.key !== "lostfamily123") {
       return new Response("Unauthorized", { status: 401 });
     }
 
-    // webhooky
     const webhook1 = "https://discord.com/api/webhooks/1479571466662973440/8efiAZLS0s4VVSAjgSUuTIO0854gV8xkJLy2GMLNxje5yBMQL5GT1m8xtIOsaIwprNYA";
     const webhook2 = "https://discord.com/api/webhooks/1479547039183405076/o6c2xOYNfL2UMLuOK7m7SxAG5CWwhcEX6Uvlwine4yIX0u5chxRuh0Y29RU5QCjBr1mi";
 
     let webhook;
 
-  if (data.type === "gielda") {
-  webhook = webhook1;
-  }
-  else if (data.type === "market") {
-  webhook = webhook2;
-  }
-    else {
+    if (data.type === "gielda") {
+      webhook = webhook1;
+    } else if (data.type === "market") {
+      webhook = webhook2;
+    } else {
       return new Response("Wrong type", { status: 400 });
     }
 
-    // wysyłanie do Discorda
-const discordResponse = await fetch(webhook, {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify(data.message)
-});
+    const discordResponse = await fetch(webhook, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data.message)
+    });
 
-const text = await discordResponse.text();
+    if (!discordResponse.ok) {
+      const err = await discordResponse.text();
+      return new Response(err, { status: 500 });
+    }
 
-return new Response(
-  "Discord status: " + discordResponse.status + "\n" + text
-);
+    return new Response("OK");
+  }
+};
